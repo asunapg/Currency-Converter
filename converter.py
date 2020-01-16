b0f77f092dcac6f60a893839ac4a094ce5d0befb
Asuna Gilfoyle <50970165+xygilfoyle@users.noreply.github.com>
2020-01-16 08:53:15 -0500

    It's finally done!

--- converter.py
+++ converter.py
@@ -7,6 +7,8 @@ supported_currencies = {
 # Currencies above here
 
 # USD below here
+
+
 def usd_to_cad():
     user_value = input("How many U.S. Dollars? ")
     amount = float(user_value)
@@ -25,17 +27,80 @@ def usd_to_gbp():
     user_value = input("How many U.S. Dollars? ")
     amount = float(user_value)
     conversion = amount * 0.77
-    print(str(user_value) + " U.S. Dollars " + str(conversion) + " Euros.")
+    print(str(user_value) + " U.S. Dollars " + str(conversion) + " GBP.")
 
 
 # USD above here
 
 # CAD below here
 def cad_to_usd():
-    user_value = input("How many U.S. Dollars? ")
+    user_value = input("How many Canadian Dollars? ")
     amount = float(user_value)
     conversion = amount * 0.77
-    print(str(user_value) + " U.S. Dollars " + str(conversion) + " Euros.")
+    print(str(user_value) + " Canadian Dollars " + str(conversion) + " U.S. Dollars.")
+
+
+def cad_to_eur():
+    user_value = input("How many Canadian Dollars? ")
+    amount = float(user_value)
+    conversion = amount * 0.69
+    print(str(user_value) + " Canadian Dollars" + str(conversion) + " Euros.")
+
+
+def cad_to_gbp():
+    user_value = input("How many Canadian Dollars? ")
+    amount = float(user_value)
+    conversion = amount * 0.59
+    print(str(user_value) + " Canadian Dollars" + str(conversion) + " GBP.")
+
+
+# CAD above here
+
+# EUR below here
+def eur_to_usd():
+    user_value = input("How many Euros? ")
+    amount = float(user_value)
+    conversion = amount * 1.12
+    print(str(user_value) + " Euros " + str(conversion) + " U.S. Dollars.")
+
+
+def eur_to_cad():
+    user_value = input("How many Euros? ")
+    amount = float(user_value)
+    conversion = amount * 1.12
+    print(str(user_value) + " Euros " + str(conversion) + " Canadian Dollars.")
+
+
+def eur_to_gbp():
+    user_value = input("How many Euros? ")
+    amount = float(user_value)
+    conversion = amount * 0.86
+    print(str(user_value) + " Euros " + str(conversion) + " GBP.")
+
+# EUR above here
+
+# GBP below here
+
+
+def gbp_to_usd():
+    user_value = input("How many Pound? ")
+    amount = float(user_value)
+    conversion = amount * 1.30
+    print(str(user_value) + " pounds " + str(conversion) + " U.S. Dollars.")
+
+
+def gbp_to_cad():
+    user_value = input("How many Pound? ")
+    amount = float(user_value)
+    conversion = amount * 1.70
+    print(str(user_value) + " pounds " + str(conversion) + " Canadian Dollars.")
+
+
+def gbp_to_eur():
+    user_value = input("How many Pound? ")
+    amount = float(user_value)
+    conversion = amount * 1.17
+    print(str(user_value) + " pounds " + str(conversion) + " Euros.")
 
 
 print("Welcome to currency converter.")
@@ -46,8 +111,41 @@ for currency in supported_currencies:
 starting_value = input("Convert: ")
 end_value = input("To: ")
 
-if starting_value == "CAD" and end_value == "USD":
-    cad_to_usd()
-
-elif starting_value == "USD" and end_value == "CAD":
+# USD conversion below here
+if starting_value == "USD" and end_value == "CAD":
     usd_to_cad()
+
+elif starting_value == "USD" and end_value == "EUR":
+    usd_to_eur()
+
+elif starting_value == "USD" and end_value == "GBP":
+    usd_to_gbp()
+# USD conversion above here
+
+# CAD conversion below here
+elif starting_value == "CAD" and end_value == "USD":
+    cad_to_usd()
+elif starting_value == "CAD" and end_value == "EUR":
+    cad_to_eur()
+elif starting_value == "CAD" and end_value == "GBP":
+    cad_to_gbp()
+# CAD conversion above here
+
+# EUR conversion below here
+elif starting_value == "EUR" and end_value == "USD":
+    eur_to_usd()
+elif starting_value == "EUR" and end_value == "CAD":
+    eur_to_cad()
+elif starting_value == "EUR" and end_value == "GBP":
+    eur_to_gbp()
+# EUR conversion above here
+
+# GBP conversion below here
+elif starting_value == "GBP" and end_value == "USD":
+    gbp_to_usd()
+elif starting_value == "GBP" and end_value == "CAD":
+    gbp_to_cad()
+elif starting_value == "GBP" and end_value == "EUR":
+    gbp_to_eur()
+# GBP conversion above here
+
